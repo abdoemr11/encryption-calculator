1a0a496aeb8e7f2865ed0574515ba7a84074cbce
abdoemr11 <abdoemr11@gmail.com>
2021-12-31 18:58:29 +0200

    finished syling remain writing logic with javascript

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import {multi_tap, progressive} from "./encryptalgorithm.js";
 // const multi_tap = require("./multi-tap.js");
 // console.log(encyrpt("abcdefghijklmnopqrstuvwxyz"));
 console.log(progressive("abdosaberz", 3));
+
+document.forms[0].addEventListener("submit", event=> {
+    console.log("button clicked");
+    event.preventDefault();
+});
